d8cd410df11b8c2f5218a42a8ccfe2f0c6ed839e
giltesa <developer@giltesa.com>
2017-05-11 21:29:39 +0200

    Add CDN of Material Design CSS/JS

--- ESP8266-Firmware/webpage/generate.sh
+++ ESP8266-Firmware/webpage/generate.sh
@@ -12,27 +12,11 @@
     xxd -i compressed/style.min.css > compressed/style
     sed -i 's/\[\]/\[\] ICACHE_STORE_ATTR ICACHE_RODATA_ATTR /g' compressed/style
 
-#BOOTSTRAP-MATERIAL-DESIGN.MIN.CSS
-    xxd -i bootstrap-material-design.min.css > compressed/materialcss
-    sed -i 's/\[\]/\[\] ICACHE_STORE_ATTR ICACHE_RODATA_ATTR /g' compressed/materialcss
-
-#RIPPLES.MIN.CSS
-    xxd -i ripples.min.css > compressed/ripplescss
-    sed -i 's/\[\]/\[\] ICACHE_STORE_ATTR ICACHE_RODATA_ATTR /g' compressed/ripplescss
-
 
 #SCRIPT.JS
     yui-compressor -o compressed/script.min.js script.js
     xxd -i compressed/script.min.js > compressed/script
     sed -i 's/\[\]/\[\] ICACHE_STORE_ATTR ICACHE_RODATA_ATTR /g' compressed/script
-
-#MATERIAL.MIN.JS
-    xxd -i material.min.js > compressed/materialjs
-    sed -i 's/\[\]/\[\] ICACHE_STORE_ATTR ICACHE_RODATA_ATTR /g' compressed/materialjs
-
-#RIPPLES.MIN.JS
-    xxd -i ripples.min.js > compressed/ripplesjs
-    sed -i 's/\[\]/\[\] ICACHE_STORE_ATTR ICACHE_RODATA_ATTR /g' compressed/ripplesjs
 
 #JQUERY.TRANSLATE.JS
     yui-compressor -o compressed/jquery.translate.min.js jquery.translate.js
