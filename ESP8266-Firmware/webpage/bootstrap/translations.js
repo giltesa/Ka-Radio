00c1b4f4cf8c796feb1fefc3ee60ad086d81994d
Andy Karpov <andy.karpov@gmail.com>
2017-05-11 11:23:31 +0300

    Added russian translations

--- ESP8266-Firmware/webpage/bootstrap/translations.js
+++ ESP8266-Firmware/webpage/bootstrap/translations.js
@@ -1,451 +1,563 @@
 var translations = {
     "Home": {           //Text in English, has to match the HTML. (If the texts are very long could overflow the mobile interface, try to have a length similar to the English/Spanish translation.)
         fr: "",         //French translation.
-        es: "Inicio"    //Spanish translation.
+        es: "Inicio",    //Spanish translation.
+        ru: "Главная"
     },
     "Stations": {
         fr: "",
-        es: "Emisoras"
+        es: "Emisoras",
+        ru: "Станции"
     },
     "Settings": {
         fr: "",
-        es: "Ajustes"
+        es: "Ajustes",
+        ru: "Настройки"
     },
     "About": {
         fr: "",
-        es: "Acerca de"
+        es: "Acerca de",
+        ru: "О программе"
     },
     "Language": {
         fr: "",
-        es: "Idioma"
+        es: "Idioma",
+        ru: "Язык"
     },
     "Select station": {
         fr: "",
-        es: "Seleccionar emisora"
+        es: "Seleccionar emisora",
+        ru: "Выбор станции"
     },
     "AutoPlay": {
         fr: "",
-        es: "Auto reprod."
+        es: "Auto reprod.",
+        ru: "Автоплей"
     },
     "Station:": {
         fr: "",
-        es: "Emisora:"
+        es: "Emisora:",
+        ru: "Станция"
     },
     "Playing:": {
         fr: "",
-        es: "Reproduc.:"
+        es: "Reproduc.:",
+        ru: "Играет:"
     },
     "Volume:": {
         fr: "",
-        es: "Volumen"
+        es: "Volumen",
+        ru: "Громкость"
     },
     "Station No:": {
         fr: "",
-        es: "Estación núm:"
+        es: "Estación núm:",
+        ru: "Станция №:"
     },
     "Full": {
         fr: "",
-        es: "Detalles"
+        es: "Detalles",
+        ru: "Детали"
     },
     "Name:": {
         fr: "",
-        es: "Nombre"
+        es: "Nombre",
+        ru: "Имя:"
     },
     "Description:": {
         fr: "",
-        es: "Descripción:"
+        es: "Descripción:",
+        ru: "Описание"
     },
     "Notice:": {
         fr: "",
-        es: "Metadatos:"
+        es: "Metadatos:",
+        ru: "Метадата:"
     },
     "Genre:": {
         fr: "",
-        es: "Género:"
+        es: "Género:",
+        ru: "Стиль:"
     },
     "Full URL:": {
         fr: "",
-        es: "URL completa:"
+        es: "URL completa:",
+        ru: "Полный URL:"
     },
     "URL:": {
         fr: "",
-        es: "URL:"
+        es: "URL:",
+        ru: "URL:"
     },
     "Bitrate:": {
         fr: "",
-        es: "Tasa de bits:"
+        es: "Tasa de bits:",
+        ru: "Битрейт"
     },
     "Monitoring": {
         fr: "",
-        es: "Reproducción local"
+        es: "Reproducción local",
+        ru: "Мониторинг"
     },
     "Volume:": {
         fr: "",
-        es: "Volumen:"
+        es: "Volumen:",
+        ru: "Громкость:"
     },
     "I'm sorry; your browser doesn't support HTML5": {
         fr: "",
-        es: "Lo siento, su navegador no admite HTML5"
+        es: "Lo siento, su navegador no admite HTML5",
+        ru: "Простите, Ваш браузер не поддерживает HTML5"
     },
     "Sleep / Wake": {
         fr: "",
-        es: "Temporizador"
+        es: "Temporizador",
+        ru: "Режим сна"
     },
     "Current time:": {
         fr: "",
-        es: "Hora actual:"
+        es: "Hora actual:",
+        ru: "Текущее время:"
     },
     "Sleep:": {
         fr: "",
-        es: "Dormir:"
+        es: "Dormir:",
+        ru: "Сон:"
     },
     "Wake:": {
         fr: "",
-        es: "Despertar:"
+        es: "Despertar:",
+        ru: "Пробуждение:"
     },
     "Instant play": {
         fr: "",
-        es: "Reproducir ahora"
+        es: "Reproducir ahora",
+        ru: "Непрерывно играть"
     },
     "Domain:": {
         fr: "",
-        es: "Dominio:"
+        es: "Dominio:",
+        ru: "Домен"
     },
     "Port:": {
         fr: "",
-        es: "Puerto:"
+        es: "Puerto:",
+        ru: "Порт:"
     },
     "Path:": {
         fr: "",
-        es: "Ruta:"
+        es: "Ruta:",
+        ru: "Путь:"
     },
     "Edit station": {
         fr: "",
-        es: "Editar emisora"
+        es: "Editar emisora",
+        ru: "Редактировать"
     },
     "Slot number:": {
         fr: "",
-        es: "Número:"
+        es: "Número:",
+        ru: "Номер:"
     },
     "Name:": {
         fr: "",
-        es: "Nombre:"
+        es: "Nombre:",
+        ru: "Имя:"
     },
     "Volume offset:": {
         fr: "",
-        es: "Compensar volumen:"
+        es: "Compensar volumen:",
+        ru: "Смещение громкости:"
     },
     "Manage stations": {
         fr: "",
-        es: "Administrar emisoras"
+        es: "Administrar emisoras",
+        ru: "Редактировать станции"
     },
     "Reloaded the stations... Please Wait": {
         fr: "",
-        es: "Recargando estaciones, por favor espere..."
+        es: "Recargando estaciones, por favor espere...",
+        ru: "Перезагрузка станций, подождите..."
     },
     "The list is modified. Do you want to save the modified list?": {
         fr: "",
-        es: "La lista ha sido modificada, ¿Guardar los cambios?"
+        es: "La lista ha sido modificada, ¿Guardar los cambios?",
+        ru: "Список изменен. Хотите сохранить изменения?"
     },
     "Karadio Stations list": {
         fr: "",
-        es: "Karadio: lista de emisoras"
+        es: "Karadio: lista de emisoras",
+        ru: "Список станций Karadio"
     },
     "Warning: This will clear all stations.": {
         fr: "",
-        es: "Advertencia: Esto borrará todas las estaciones."
+        es: "Advertencia: Esto borrará todas las estaciones.",
+        ru: "Предупреждение: Это очистит весь список станций."
     },
     "Be sure to save station before.": {
         fr: "",
-        es: "Asegúrese de guardar las estaciones antes."
+        es: "Asegúrese de guardar las estaciones antes.",
+        ru: "Сохраните станцию перед этим."
     },
     "Clear now?": {
         fr: "",
-        es: "¿Borrarlas ahora?"
+        es: "¿Borrarlas ahora?",
+        ru: "Очистить?"
     },
     "No.": {
         fr: "",
-        es: "#"
+        es: "#",
+        ru: "№"
     },
     "Name": {
         fr: "",
-        es: "Nombre"
+        es: "Nombre",
+        ru: "Имя"
     },
     "URL": {
         fr: "",
-        es: "URL"
+        es: "URL",
+        ru: "URL"        
     },
     "Path": {
         fr: "",
-        es: "Ruta"
+        es: "Ruta",
+        ru: "Путь"
     },
     "Port": {
         fr: "",
-        es: "Puerto"
+        es: "Puerto",
+        ru: "Порт"
     },
     "Vol": {
         fr: "",
-        es: "Vol."
+        es: "Vol.",
+        ru: "Громк."
     },
     "Sound Setting": {
         fr: "",
-        es: "Configuración de sonido"
+        es: "Configuración de sonido",
+        ru: "Настройки звука"
     },
     "Treble control:": {
         fr: "",
-        es: "Control agudos:"
+        es: "Control agudos:",
+        ru: "Настройки тембра:"
     },
     "Treble freq:": {
         fr: "",
-        es: "Frecuencia agudos:"
+        es: "Frecuencia agudos:",
+        ru: "Частота тембра:"
     },
     "Bass enhance:": {
         fr: "",
-        es: "Potenciar graves:"
+        es: "Potenciar graves:",
+        ru: "Улучшение басов:"
     },
     "Bass freq:": {
         fr: "",
-        es: "Frecuencia graves:"
+        es: "Frecuencia graves:",
+        ru: "Частота басов:"
     },
     "Spacialization:": {
         fr: "",
-        es: "Espacialización:"
+        es: "Espacialización:",
+        ru: "Специализация:"
     },
     "Off": {
         fr: "",
-        es: "Apagado"
+        es: "Apagado",
+        ru: "Выкл."
     },
     "Minimal": {
         fr: "",
-        es: "Mínimo"
+        es: "Mínimo",
+        ru: "Минимум"
     },
     "Normal": {
         fr: "",
-        es: "Normal"
+        es: "Normal",
+        ru: "Нормально"
     },
     "Maximal": {
         fr: "",
-        es: "Máximo"
+        es: "Máximo",
+        ru: "Максимум"
     },
     "Volume:": {
         fr: "",
-        es: "Volumen:"
+        es: "Volumen:",
+        ru: "Громкость:"
     },
     "Stations Save y Restore": {
         fr: "",
-        es: "Guardar y restaurar emisoras"
+        es: "Guardar y restaurar emisoras",
+        ru: "Сохранение и восстановление станций"
     },
     "Download:": {
         fr: "",
-        es: "Descargar:"
+        es: "Descargar:",
+        ru: "Скачивание:"
     },
     "Download": {
         fr: "",
-        es: "Descargar"
+        es: "Descargar",
+        ru: "Скачать"
     },
     "Restore:": {
         fr: "",
-        es: "Restaurar:"
+        es: "Restaurar:",
+        ru: "Восстановление"
     },
     "Restore": {
         fr: "",
-        es: "Restaurar"
+        es: "Restaurar",
+        ru: "Восстановить"
     },
     "Please select a file": {
         fr: "",
-        es: "Por favor, seleccione un archivo"
+        es: "Por favor, seleccione un archivo",
+        ru: "Пожалуйста, выберите файл"
     },
     "WiFi y Network": {
         fr: "",
-        es: "Configuración WiFi"
+        es: "Configuración WiFi",
+        ru: "Настройка WiFi"
     },
     "System reboot... Please Wait": {
         fr: "",
-        es: "Karadio reiniciandose, por favor espere..."
+        es: "Karadio reiniciandose, por favor espere...",
+        ru: "Перезагрузка... Подождите, пожалуйста"
     },
     "WiFi:": {
         fr: "",
-        es: "WiFi:"
+        es: "WiFi:",
+        ru: "WiFi:"
     },
     "Password:": {
         fr: "",
-        es: "Contraseña:"
+        es: "Contraseña:",
+        ru: "Пароль:"
     },
     "Mac Address:": {
         fr: "",
-        es: "Dirección MAC:"
+        es: "Dirección MAC:",
+        ru: "MAC-адрес:"
     },
     "Use DHCP:": {
         fr: "",
-        es: "Usar DHCP:"
+        es: "Usar DHCP:",
+        ru: "Использовать DHCP:"
     },
     "IP:": {
         fr: "",
-        es: "IP:"
+        es: "IP:",
+        ru: "IP:"
     },
     "Mask:": {
         fr: "",
-        es: "Máscara:"
+        es: "Máscara:",
+        ru: "Маска:"
     },
     "Gateway:": {
         fr: "",
-        es: "Puerta:"
+        es: "Puerta:",
+        ru: "Шлюз:"
     },
     "HTML Option": {
         fr: "",
-        es: "Opción HTML"
+        es: "Opción HTML",
+        ru: "Опции HTML"
     },
     "User Agent:": {
         fr: "",
-        es: "Agente de usuario:"
+        es: "Agente de usuario:",
+        ru: "Пользовательский агент:"
     },
     "Web Options": {
         fr: "",
-        es: "Opciones Web"
+        es: "Opciones Web",
+        ru: "Опции Web"
     },
     "Show a popup when new firmware is available.": {
         fr: "",
-        es: "Mostrar un aviso cuando haya un nuevo firmware disponible."
+        es: "Mostrar un aviso cuando haya un nuevo firmware disponible.",
+        ru: "Показывать уведомление, когда появится новая версия прошивки."
     },
     "Theme color:": {
         fr: "",
-        es: "Color web:"
+        es: "Color web:",
+        ru: "Цветовая тема:"
     },
     "Upgrade firmware": {
         fr: "",
-        es: "Actualizar firmware"
+        es: "Actualizar firmware",
+        ru: "Обновление прошивки"
     },
     "Installed:": {
         fr: "",
-        es: "Instalado:"
+        es: "Instalado:",
+        ru: "Установлено:"
     },
     "Warning!": {
         fr: "",
-        es: "¡Advertencia!"
+        es: "¡Advertencia!",
+        ru: "Внимание!"
     },
     "If you experiment some strange problems with karadio, please check if the adc (A0) pin is wired to Ground if you don't have a control panel.": {
         fr: "",
-        es: "Si experimenta un comportamiento extraño con karadio, compruebe que el pin adc (A0) este conectado a tierra si no tiene un panel de control."
+        es: "Si experimenta un comportamiento extraño con karadio, compruebe que el pin adc (A0) este conectado a tierra si no tiene un panel de control.",
+        ru: "Хьюстон, у нас проблемы! Проверьте, пожалуйста, соединен ли аналоговый пин A0 с землей, если не подключена панель кнопок."
     },
     "To update or reload the current release:": {
         fr: "",
-        es: "Para actualizar o volver a cargar la versión actual:"
+        es: "Para actualizar o volver a cargar la versión actual:",
+        ru: "Чтобы обновить или перезагрузить текущий релиз:"
     },
     "Upgrade now": {
         fr: "",
-        es: "Actualizar ahora"
+        es: "Actualizar ahora",
+        ru: "Обновить сейчас"
     },
     "Updating the firmware, please wait...": {
         fr: "",
-        es: "Actualizando el firmware, por favor espere..."
+        es: "Actualizando el firmware, por favor espere...",
+        ru: "Обновляется прошивка, пожалуйста подождите..."
     },
     "New firmware %1 available!": {
         fr: "",
-        es: "¡Nuevo firmware %1 disponible!"
+        es: "¡Nuevo firmware %1 disponible!",
+        ru: "Доступна новая прошивка %1!"
     },
     "Contributors": {
         fr: "",
-        es: "Colaboradores"
+        es: "Colaboradores",
+        ru: "Участники"
     },
     "Author": {
         fr: "",
-        es: "Autor"
+        es: "Autor",
+        ru: "Автор"
     },
     "Developing": {
         fr: "",
-        es: "Desarrollando"
+        es: "Desarrollando",
+        ru: "Разработка"
     },
     "ESP8266-WebRadio: WiFi shoutcast player based on ESP8266 chip.": {
         fr: "",
-        es: "ESP8266-WebRadio: reproductor WiFi de shoutcast basado en el chip ESP8266."
+        es: "ESP8266-WebRadio: reproductor WiFi de shoutcast basado en el chip ESP8266.",
+        ru: "ESP8266-WebRadio: WiFi плеер потокового вещания на базе модуля ESP8266."
     },
     "Ka-Radio, a WiFi shoutcast player based on ESP8266 and VS1053b chips.": {
         fr: "",
-        es: "Ka-Radio, un reproductor WiFi de shoutcast basado en los chips ESP8266 y VS1053b."
+        es: "Ka-Radio, un reproductor WiFi de shoutcast basado en los chips ESP8266 y VS1053b.",
+        ru: "Ka-Radio, WiFi плеер потокового вещания на базе модуля ESP8266 и VS1053b."
     },
     "New web interface with Bootstrap and Material Design theme.": {
         fr: "",
-        es: "Nueva interfaz web con Bootstrap e interfaz Material Design."
+        es: "Nueva interfaz web con Bootstrap e interfaz Material Design.",
+        ru: "Новый веб-интерфейс на базе Bootstrap и темы Material Design."
     },
     "Links": {
         fr: "",
-        es: "Enlaces"
+        es: "Enlaces",
+        ru: "Ссылки"
     },
     "Donate": {
         fr: "",
-        es: "Donaciones"
+        es: "Donaciones",
+        ru: "Пожертвование"
     },
     "If you like Karadio and want to contribute to the development (software and hardware), please donate, the amount is up to you.": {
         fr: "",
-        es: ""
+        es: "",
+        ru: "Если Вам нравится Karadio и Вы хотите помочь разработке (софта и железа), пожалуйста, помогите деньгами, сумма - на Ваше усмотрение."
     },
     "Even one euro or $, I am so happy to know how many users appreciate this project.": {
         fr: "",
-        es: ""
+        es: "",
+        ru: "Даже если это будет один бакс или евро, Я буду рад."
     },
     "Thanks.": {
         fr: "",
-        es: "Gracias."
+        es: "Gracias.",
+        ru: "Спасибо."
     },
     "Playlists": {
         fr: "",
-        es: "Listas de reproducción"
+        es: "Listas de reproducción",
+        ru: "Плейлисты"
     },
     "Description": {
         fr: "",
-        es: "Descripción"
+        es: "Descripción",
+        ru: "Описание"
     },
     "Date": {
         fr: "",
-        es: "Fecha"
+        es: "Fecha",
+        ru: "Дата"
     },
     "Updated": {
         fr: "",
-        es: "Actualizado"
+        es: "Actualizado",
+        ru: "Обновлено"
     },
     "New": {
         fr: "",
-        es: "Nuevo"
+        es: "Nuevo",
+        ru: "Новый"
     },
     "Your prototypes": {
         fr: "",
-        es: "Sus prototipos"
+        es: "Sus prototipos",
+        ru: "Ваши прототипы"
     },
     "See your prototypes": {
         fr: "",
-        es: "Vea sus prototipos"
+        es: "Vea sus prototipos",
+        ru: "Смотреть Ваши прототипы"
     },
     "Close": {
         fr: "",
-        es: "Cerrar"
+        es: "Cerrar",
+        ru: "Закрыть"
     },
     "Version": {
         fr: "",
-        es: "Versión"
+        es: "Versión",
+        ru: "Версия"
     },
     "Third-party libraries": {
         fr: "",
-        es: "Librerías de terceros"
+        es: "Librerías de terceros",
+        ru: "Сторонние библиотеки"
     },
     "History": {
         fr: "",
-        es: "Historial"
+        es: "Historial",
+        ru: "История"
     },
     "Working... Please Wait": {
         fr: "",
-        es: "Trabajando, por favor espere..."
+        es: "Trabajando, por favor espere...",
+        ru: "Идет обработка, подождите..."
     },
     "Error, try again": {
         fr: "",
-        es: "Error, vuelva a intentarlo"
+        es: "Error, vuelva a intentarlo",
+        ru: "Ошибка, попробуйте еще раз"
     },
     "Started, Good night!": {
         fr: "",
-        es: "Activado, !buenas noches!"
+        es: "Activado, !buenas noches!",
+        ru: "Запущено, спокойной ночи!"
     },
     "Started": {
         fr: "",
-        es: "Activado"
+        es: "Activado",
+        ru: "Запущено"
     }
 
 };
